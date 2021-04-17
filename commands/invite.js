47676b780781888f7e7d63c48861d0b533c1717e
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2021-04-17 14:19:30 -0300

    Delete commands directory

--- commands/invite.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js"); 
-
-module.exports.help = {
-    name:"invite"
-  }
-  exports.run = async (client, message, args) => {
-    message.reply('https://discord.com/oauth2/authorize?client_id=816515410441535518&permissions=8&scope=bot');
-  }
